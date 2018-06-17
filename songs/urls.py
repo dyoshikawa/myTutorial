361bb0179165546b606e82c247360f59ee0c1755
dyoshikawa <dyoshikawa1993@gmail.com>
2018-06-17 13:56:19 +0900

    a

--- songs/urls.py
+++ songs/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
 from . import views
-import songs.views
 
 app_name = 'songs'
 urlpatterns = [
-    path('/', songs.views.index, name='index'),
+    path('/', views.index, name='index'),
 ]
